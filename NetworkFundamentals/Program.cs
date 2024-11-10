03a4e6ddc53de6b0da72319bf48bf8137e5c7ba0
Onur Eymen <onurceymen@yahoo.com>
2024-11-11 01:42:53 +0300

    WebSocket Denemesi yapıldı

--- NetworkFundamentals/Program.cs
+++ NetworkFundamentals/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using NetworkFundamentals;
+
+var webSocketServer = new WebSocketServer("http://localhost:5000/ws/");
+await webSocketServer.StartAsync();
---
+++ NetworkFundamentals/WebSocketServer.cs
@@ -0,0 +1,61 @@
+﻿using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+
+namespace NetworkFundamentals
+{
+    public class WebSocketServer
+    {
+        private readonly HttpListener _listener;
+
+        public WebSocketServer(string url)
+        {
+            _listener = new HttpListener();
+            _listener.Prefixes.Add(url);
+        }
+
+        public async Task StartAsync()
+        {
+            _listener.Start();
+            Console.WriteLine("WebSocket server is running on ws://localhost:5000/ws/");
+
+            while (true)
+            {
+                HttpListenerContext context = await _listener.GetContextAsync();
+                if (context.Request.IsWebSocketRequest)
+                {
+                    HttpListenerWebSocketContext webSocketContext = await context.AcceptWebSocketAsync(null);
+                    WebSocket webSocket = webSocketContext.WebSocket;
+
+                    Console.WriteLine("Client connected");
+                    await SendMessages(webSocket);
+                }
+                else
+                {
+                    context.Response.StatusCode = 400;
+                    context.Response.Close();
+                }
+            }
+        }
+
+        private static async Task SendMessages(WebSocket webSocket)
+        {
+            var buffer = new byte[1024 * 4];
+            while (webSocket.State == WebSocketState.Open)
+            {
+                string message = "Serverden Anlık Mesaj:" + DateTime.Now.ToString();
+                byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+
+                // Mesajı WebSocket aracılığıyla gönderiyoruz.
+                await webSocket.SendAsync(new ArraySegment<byte>(messageBytes), WebSocketMessageType.Text, true, CancellationToken.None);
+
+                Console.WriteLine("Mesaj gönderildi: " + message);
+
+                // 5 saniyede bir mesaj gönderelim
+                await Task.Delay(5000);
+            }
+        }
+
+
+    }
+}
